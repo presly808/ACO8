584c1c741351d8650bb5e31dc6b6999a7d841810
presly808 <presly808@gmail.com>
2015-10-03 15:45:01 +0300

    week3 day1 part1

---
+++ src/ua/artcode/week3/day1/company/Coder.java
@@ -0,0 +1,34 @@
+package ua.artcode.week3.day1.company;
+
+/**
+ * Created by serhii on 10/3/15.
+ */
+public class Coder extends Employee {
+
+    private String language;
+
+    public Coder() {
+        super();
+        language = "Java";
+    }
+
+    private void testAccessToParent(){
+        setAge(23);
+        getName();
+
+        super.setAge(23);
+        super.getName();
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void writeCode(){
+        System.out.println("coder writes code");
+    }
+}
---
+++ src/ua/artcode/week3/day1/company/TestInheritance.java
@@ -0,0 +1,24 @@
+package ua.artcode.week3.day1.company;
+
+/**
+ * Created by serhii on 10/3/15.
+ */
+public class TestInheritance {
+
+
+    public static void main(String[] args) {
+        Coder coder = new Coder();
+
+        coder.writeCode();
+        coder.work();
+
+        Employee employee = new Employee();
+        employee.work();
+
+
+        // Abstraction
+
+
+
+    }
+}
